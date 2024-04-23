7ea6a8b6638445857677f02a5590e937e990e883
Nilesh Solanki <nileshs2398@gmail.com>
2024-04-23 20:11:26 +0530

    update

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args){
         int i =10;
         int j;
-        
+                
         if(i==10){
             j = 20;
         }else{
