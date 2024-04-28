06a16c56260e4b2b39fdccbb1d741ba8e120796f
Nilesh Solanki <nileshs2398@gmail.com>
2024-04-28 20:51:57 +0530

    spring app

--- Hibernate-Maven/src/main/java/com/masai/dao/AccountDaoImpl.java
+++ Hibernate-Maven/src/main/java/com/masai/dao/AccountDaoImpl.java
@@ -2,6 +2,7 @@ package com.masai.dao;
 
 import com.masai.model.Account;
 
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
--- Hibernate-Maven/src/main/java/com/masai/useCase/Demo.java
+++ Hibernate-Maven/src/main/java/com/masai/useCase/Demo.java
@@ -1,6 +1,7 @@
 package com.masai.useCase;
 
 import com.masai.dao.AccountDao;
+
 import com.masai.dao.AccountDaoImpl;
 import com.masai.model.Account;
 
@@ -11,6 +12,7 @@ public class Demo {
 		Account acc1 = new Account("Nilesh", 1000);
 		
 		Account acc2 = new Account("Suresh", 2000);
+//		acc2.setAccNo(1);
 		
 		Account acc3 = new Account("Pritesh", 3000);
 		
